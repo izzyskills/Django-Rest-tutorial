10766edfbc294af2ca9e857d220814fad906ee91
Israel Omola <izzyskills@gmail.com>
2023-10-01 08:49:57 +0100

    part5

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
 from snippets.models import Snippet
+from django.contrib.auth.models import User
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name="snippet-highlight", format="html"
+    )
+
     class Meta:
         model = Snippet
-        fields = ["id", "title", "code", "linenos"]
+        fields = ["id", "title", "code", "linenos", "owner", "highlight"]
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name="snippet-detail", read_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "snippets"]
--- snippets/views.py
+++ snippets/views.py
@@ -1,54 +1,52 @@
-# from rest_framework import status
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# from snippets.models import Snippet
-# from snippets.serializers import SnippetSerializer
-
-
-# @api_view(["GET", "POST"])
-# def snippet_list(request, format=None):
-#     if request.method == "GET":
-#         snippets = Snippet.objects.all()
-#         serializer = SnippetSerializer(snippets, many=True)
-#         return Response(serializer.data)
-#     elif request.method == "POST":
-#         serializer = SnippetSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# @api_view(["GET", "PUT", "DELETE"])
-# def snippet_detail(request, pk, format=None):
-#     try:
-#         snippet = Snippet.objects.get(pk=pk)
-#     except Snippet.DoesNotExist:
-#         return Response(status=status.HTTP_400_BAD_REQUEST)
-
-#     if request.method == "GET":
-#         serializer = SnippetSerializer(snippet)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-#     elif request.method == "PUT":
-#         serializer = SnippetSerializer(snippet, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#     elif request.method == "DELETE":
-#         snippet.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
-from rest_framework import generics
+from snippets.serializers import SnippetSerializer, UserSerializer
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from django.contrib.auth.models import User
+from snippets.permissions import isOwnerOrReadOnly
+
+
+@api_view["GET"]
+def api_root(request, format=None):
+    return Response(
+        {
+            "users": reverse("user-list", request=request, format=format),
+            "snippets": reverse("snippet-list", request=request, format=format),
+        }
+    )
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, isOwnerOrReadOnly]
     serializer_class = SnippetSerializer
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
